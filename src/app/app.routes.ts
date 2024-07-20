42623eb363018ce4745201d3af22c40102089f5f
Alison <112625415+Alison-Doyle@users.noreply.github.com>
2024-07-20 11:24:48 +0100

    :sparkles: Created side menu ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Application components
+import { HomeComponent } from './components/home/home/home.component';
+import { SettingsComponent } from './components/settings/settings/settings.component';
+import { CreateNewProjectComponent } from './components/create-new-project/create-new-project/create-new-project.component';
+import { ProjectDetailsComponent } from './components/project-details/project-details/project-details.component';
+
+export const routes: Routes = [
+    { path: "", component: HomeComponent }
+    , { path: "settings", component: SettingsComponent }
+    , { path: "create-new-project", component: CreateNewProjectComponent }
+    , { path: "project-details", component: ProjectDetailsComponent }
+];
---
+++ src/app/components/create-new-project/create-new-project/create-new-project.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-project',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-new-project.component.html',
+  styleUrl: './create-new-project.component.scss'
+})
+export class CreateNewProjectComponent {
+
+}
